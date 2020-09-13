de3937193453e295a0cb07bc61fe9e9fe2be1842
Ivan Podorozhnyi <ivanpodorozhnj@gmail.com>
2020-09-13 23:30:40 +0300

    Автоматизируем сборку личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const del = require("del");
 const sync = require("browser-sync").create();
 
-// Styles
 
+// Styles
 const styles = () => {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -16,11 +19,12 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
-
 exports.styles = styles;
 
 // Server
@@ -28,7 +32,7 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -46,6 +50,40 @@ const watcher = () => {
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
+// copy
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+}
+exports.copy = copy;
+
+const clean = () => {
+  return del("build");
+}
+exports.clean = clean;
+
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"))
+}
+const build = gulp.series(
+  clean,
+  copy,
+  styles,
+  html
 );
+
+exports.build = build;
+
+exports.default = gulp.series(
+  build, server, watcher
+);
+
